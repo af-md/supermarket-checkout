e040a37e73e35b0b090193eb5441d0063f916719
Afzal Muhammad <afzaalm059@gmail.com>
2025-07-09 16:46:14 +0100

    Implement error handling for Scan method

--- checkout/checkout.go
+++ checkout/checkout.go
@@ -1,5 +1,12 @@
 package checkout
 
+import "errors"
+
+const (
+	EmptySKUError      = "SKU cannot be empty"
+	WhitespaceSKUError = "SKU cannot have whitespace"
+)
+
 type ICheckout interface {
 	Scan(SKU string) (err error)
 	GetTotalPrice() (totalPrice int, err error)
@@ -20,6 +27,15 @@ func NewCheckout() *checkout {
 }
 
 func (c *checkout) Scan(SKU string) error {
+	// could have trimmed the whitespace here, but I wanted to show how I would handle multiple edge cases
+	if SKU == "" {
+		return errors.New(EmptySKUError)
+	}
+
+	if SKU == " " {
+		return errors.New(WhitespaceSKUError)
+	}
+
 	c.items = append(c.items, SKU)
 	return nil
 }
--- checkout/checkout_test.go
+++ checkout/checkout_test.go
@@ -21,3 +21,39 @@ func TestScanItem(t *testing.T) {
 		t.Errorf("Expected item 'A', got %s", checkout.items[0])
 	}
 }
+
+
+// Negatvie edge cases tests for Scan method
+func TestScanEdgeCases(t *testing.T) {
+	tests := []struct {
+		name     string
+		sku      string
+		errorMsg string
+	}{
+		{
+			name:     "empty SKU",
+			sku:      "",
+			errorMsg: "SKU cannot be empty",
+		},
+		{
+			name:     "whitespace only SKU",
+			sku:      " ",
+			errorMsg: "SKU cannot have whitespace",
+		},
+		// further edge cases can be added here but in the interest of time I will keep it simple
+	}
+
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			checkout := NewCheckout()
+			
+			err := checkout.Scan(tt.sku)
+			
+			if err == nil {
+				t.Errorf("Expected error for %s, got nil", tt.name)
+			} else if err.Error() != tt.errorMsg {
+				t.Errorf("Expected error message '%s', got '%s'", tt.errorMsg, err.Error())
+			}
+		})
+	}
+}
